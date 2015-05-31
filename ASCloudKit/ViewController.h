e223bf16258e21690d937965519d3dd41f707c31
Hasan Adil <@dispatch_hasan>
2015-05-31 11:20:24 -0400

    Initial Commit

---
+++ ASCloudKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASCloudKit
+//
+//  Created by Hasan Adil on 5/31/15.
+//  Copyright (c) 2015 Assemble Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ASCloudKit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ASCloudKit
+//
+//  Created by Hasan Adil on 5/31/15.
+//  Copyright (c) 2015 Assemble Labs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
